d68c189bc46aabb6a2426c49e3bb5f69656ad5ba
Ismail Karmouche <ismail.karmouche@etudiant.univ-lr.fr>
2020-12-06 18:42:26 +0100

    ajout controller iteration 1

---
+++ mpapws/src/Controller/InfosProduitController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class InfosProduitController extends AbstractController
+{
+    /**
+     * @Route("/infos/produit", name="infos_produit")
+     */
+    public function index(): Response
+    {
+        return $this->render('infos_produit/index.html.twig', [
+            'controller_name' => 'InfosProduitController',
+        ]);
+    }
+}

c2bea89a5ad2acb7f1c5d5a366d72fc813bed1d3
Killian Biou <killian.biou@etudiant.univ-lr.fr>
2020-12-08 15:16:38 +0100

    Ajout des elements doctrine

---
+++ mpapws/src/Entity/Evenement.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EvenementRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EvenementRepository::class)
+ */
+class Evenement
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $nomEvt;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $idProducteur;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $detailEvt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomEvt(): ?string
+    {
+        return $this->nomEvt;
+    }
+
+    public function setNomEvt(string $nomEvt): self
+    {
+        $this->nomEvt = $nomEvt;
+
+        return $this;
+    }
+
+    public function getIdProducteur(): ?int
+    {
+        return $this->idProducteur;
+    }
+
+    public function setIdProducteur(int $idProducteur): self
+    {
+        $this->idProducteur = $idProducteur;
+
+        return $this;
+    }
+
+    public function getDetailEvt(): ?string
+    {
+        return $this->detailEvt;
+    }
+
+    public function setDetailEvt(string $detailEvt): self
+    {
+        $this->detailEvt = $detailEvt;
+
+        return $this;
+    }
+}

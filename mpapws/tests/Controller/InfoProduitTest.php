570f22bff32fb98e7c9dc7a871da4e9fc00a46f2
Ismail Karmouche <ismail.karmouche@etudiant.univ-lr.fr>
2021-01-18 00:58:58 +0100

    Update InfoProduitTest.php

--- mpapws/tests/Controller/InfoProduitTest.php
+++ mpapws/tests/Controller/InfoProduitTest.php
@@ -2,6 +2,9 @@
 
 namespace App\Tests\Controller;
 
+use App\Entity\Exploitation;
+use App\Entity\Produit;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class InfoProduitTest extends WebTestCase
@@ -21,7 +24,7 @@ class InfoProduitTest extends WebTestCase
     /**
      * Fonction de test affichage valeurs de la page
      */
-    public function testInfosProduits()
+    public function testInfosProduits(ObjectManager $entityManager)
     {
         $client = static::createClient();
 
@@ -29,8 +32,25 @@ class InfoProduitTest extends WebTestCase
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
 
-        // On teste
-        $this->assertSelectorTextContains('html body div div h1', 'Fromage de chevre');
+        $exploitationTest = new Exploitation();
+        $exploitationTest->setNom("NomTest");
+        $exploitationTest->setAdresse("Adresse");
+        $exploitationTest->setDetails("details");
+        $exploitationTest->setIdExploitant(1);
+        $entityManager->persist($exploitationTest);
+
+
+        $produitTest = new Produit();
+        $produitTest->setId(1);
+        $produitTest->setIdExploitation(1);
+        $produitTest->setNom("TEST");
+        $produitTest->setCategorie("categorieTest");
+        $produitTest->setDescription("TestDescription");
+
+        $entityManager->persist($produitTest);
+        $entityManager->flush();
+
+        $this->assertSelectorTextContains('html body div div h1', 'TEST');
 
     }
 }
